6560871ba73ce28e9ff394be1fb3a6fc626c32bf
adrianofaletar <adriano.faletar@fer.unizg.hr>
2025-01-20 22:48:58 +0100

    add getters and setters

--- src/main/java/com/af/moslavac/dtos/NotificationRequest.java
+++ src/main/java/com/af/moslavac/dtos/NotificationRequest.java
@@ -17,4 +17,36 @@ public class NotificationRequest {
     private String body;
     private String icon;
 
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
 }
--- src/main/java/com/af/moslavac/entities/News.java
+++ src/main/java/com/af/moslavac/entities/News.java
@@ -38,4 +38,52 @@ public class News {
     protected void onCreate() {
         this.date = LocalDateTime.now();
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getThumbnailPath() {
+        return thumbnailPath;
+    }
+
+    public void setThumbnailPath(String thumbnailPath) {
+        this.thumbnailPath = thumbnailPath;
+    }
+
+    public List<String> getImagePaths() {
+        return imagePaths;
+    }
+
+    public void setImagePaths(List<String> imagePaths) {
+        this.imagePaths = imagePaths;
+    }
 }
