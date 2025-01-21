9d6d62a647d1ca4847d6c87594357eb3713623ee
adrianofaletar <adriano.faletar@fer.unizg.hr>
2025-01-21 09:23:12 +0100

    deploy debug

--- src/main/java/com/af/moslavac/Moslavac.java
+++ src/main/java/com/af/moslavac/Moslavac.java
@@ -2,9 +2,8 @@ package com.af.moslavac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication()
 public class Moslavac {
 
 	public static void main(String[] args) {
