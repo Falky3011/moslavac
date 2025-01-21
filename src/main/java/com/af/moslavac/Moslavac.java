a3b292fe4306a8b647f616355eac2f05e8aab688
adrianofaletar <adriano.faletar@fer.unizg.hr>
2025-01-21 08:51:57 +0100

    data source auto configuration

--- src/main/java/com/af/moslavac/Moslavac.java
+++ src/main/java/com/af/moslavac/Moslavac.java
@@ -2,9 +2,10 @@ package com.af.moslavac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-public class  Moslavac {
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class Moslavac {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Moslavac.class, args);
